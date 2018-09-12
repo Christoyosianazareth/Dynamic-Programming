8ac52cfdf140a3591d8ac8efc1ec30920d71dd1b
GL553VD <GL553VD@DESKTOP-I019UBP>
2018-09-12 13:13:01 +0700

    Tambah Tampil Hasil

--- src/latihan1/L1.java
+++ src/latihan1/L1.java
@@ -21,6 +21,7 @@ public class L1 {
         tampilJudul(identitas);
         int n=tampilInput();
         BigInteger hasil=fibo(n);
+        tampilHasil(n, hasil);
     }
     private static int tampilInput(){
         Scanner scanner = new Scanner (System.in);
@@ -39,5 +40,7 @@ for(int i = 2; i < n; i++){
 }
 return hasil[n-1];
 }
-
+private static void tampilHasil(int n, BigInteger hasil){
+    System.out.println("Bilangan Fibonacci ke-"+n+":"+hasil);
+}
 }
