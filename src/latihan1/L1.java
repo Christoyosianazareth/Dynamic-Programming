107947d5c89f61c4fe8974f27e0b4de11d573e6a
GL553VD <GL553VD@DESKTOP-I019UBP>
2018-09-12 12:57:18 +0700

    Initial Commit

---
+++ src/latihan1/L1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+import java.util.Scanner;
+/**
+ *
+ * @author GL553VD
+ */
+public class L1 {
+    private static void tampilJudul(String identitas){
+        System.out.println("Identitas:"+identitas);
+        System.out.println("\nHitung Fibonnaci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    public static void main(String []aargs){
+        String identitas="Christo Yosia Nazareth/09/XRPL5";
+        tampilJudul(identitas); 
+    }
+    
+
+}
