7f89d7919da44338dcf949b8cb8a2513fbc7bc0d
GL553VD <GL553VD@DESKTOP-I019UBP>
2018-09-12 13:10:36 +0700

    Tambah Hitung Fibonacci

--- src/latihan1/L1.java
+++ src/latihan1/L1.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package latihan1;
+import java.math.BigInteger;
 import java.util.Scanner;
 /**
  *
@@ -17,7 +18,9 @@ public class L1 {
     }
     public static void main(String []aargs){
         String identitas="Christo Yosia Nazareth/09/XRPL5";
-        tampilJudul(identitas); 
+        tampilJudul(identitas);
+        int n=tampilInput();
+        BigInteger hasil=fibo(n);
     }
     private static int tampilInput(){
         Scanner scanner = new Scanner (System.in);
@@ -25,6 +28,16 @@ public class L1 {
         int n = scanner.nextInt();
         return n;
     }
-int n=tampilInput();
+
+
+private static BigInteger fibo(int n){
+    BigInteger[]hasil=new BigInteger[n];
+hasil[0]= BigInteger.ONE;
+hasil[1]= BigInteger.ONE;
+for(int i = 2; i < n; i++){
+    hasil[i]=hasil[i-1].add(hasil[i-2]);
+}
+return hasil[n-1];
+}
 
 }
